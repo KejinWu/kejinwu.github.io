97399a092a24c88a1e3afc75147be514c6f148ea
KKKKKJ <kwu@ucsd.edu>
2025-11-27 21:57:47 -0800

    test

--- docs/assets/js/data/swconf.js
+++ docs/assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1764020995',resources: [
+    cacheName: 'chirpy-1764309424',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
