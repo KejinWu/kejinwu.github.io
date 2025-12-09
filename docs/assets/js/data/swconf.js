35c68abbd9cbe47b89a8e53c8ba1eef8e699f897
KKKKKJ <kwu@ucsd.edu>
2025-12-08 22:21:12 -0600

    googleID

--- docs/assets/js/data/swconf.js
+++ docs/assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1765207779',resources: [
+    cacheName: 'chirpy-1765254021',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
