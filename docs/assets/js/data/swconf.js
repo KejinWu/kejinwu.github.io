9e37b2842f88651f5d677de5c272856f5bff6f4c
KKKKKJ <kwu@ucsd.edu>
2026-01-12 17:38:18 -0600

    addID

--- docs/assets/js/data/swconf.js
+++ docs/assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1767910579',resources: [
+    cacheName: 'chirpy-1768261058',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
