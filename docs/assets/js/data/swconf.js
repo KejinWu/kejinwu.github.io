3d67fa641f166e212acfba62a9525667855c48c7
KKKKKJ <kwu@ucsd.edu>
2026-02-12 11:51:50 -0600

    addinvitation

--- docs/assets/js/data/swconf.js
+++ docs/assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1768524449',resources: [
+    cacheName: 'chirpy-1770918672',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
