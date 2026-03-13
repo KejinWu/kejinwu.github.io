81f27bafcbea92646bf5e89ddd872cea89e94714
KKKKKJ <kwu@ucsd.edu>
2026-03-12 22:42:37 -0500

    addIMSaward

--- docs/assets/js/data/swconf.js
+++ docs/assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1770931569',resources: [
+    cacheName: 'chirpy-1773373319',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
